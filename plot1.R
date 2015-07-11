c8d03f00869fe8e62224b303588a8917180a8c5a
usefuldata <u.datta@tcs.com>
2015-07-11 23:35:54 +0530

    Assignment #3

---
+++ plot1.R
@@ -0,0 +1,12 @@
+t <- read.table("household_power_consumption.txt", header = TRUE, sep = ";",stringsAsFactors=F)
+t <- t[which(t$Date == '1/2/2007' | t$Date == '2/2/2007'),] 
+t$dT <- strptime(paste(t$Date, t$Time, sep = " : "), format = "%e/%m/%Y : %H:%M:%S") 
+t$Global_active_power <- as.numeric(t$Global_active_power) 
+t$Global_reactive_power <- as.numeric(t$Global_reactive_power) 
+t$Global_intensity <- as.numeric(t$Global_intensity) 
+t$Sub_metering_1 <- as.numeric(t$Sub_metering_1)
+t$Sub_metering_2 <- as.numeric(t$Sub_metering_2) 
+t$Sub_metering_3 <- as.numeric(t$Sub_metering_3) 
+png("plot1.png")
+hist(t$Global_active_power, col = 'red',main = 'Global Active Power', xlab = 'Global Active Power (kilowatts)')
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,13 @@
+t <- read.table("household_power_consumption.txt", header = TRUE, sep = ";",stringsAsFactors=F)
+t <- t[which(t$Date == '1/2/2007' | t$Date == '2/2/2007'),] 
+t$dT <- strptime(paste(t$Date, t$Time, sep = " : "), format = "%e/%m/%Y : %H:%M:%S") 
+t$Global_active_power <- as.numeric(t$Global_active_power) 
+t$Global_reactive_power <- as.numeric(t$Global_reactive_power) 
+t$Global_intensity <- as.numeric(t$Global_intensity) 
+t$Sub_metering_1 <- as.numeric(t$Sub_metering_1)
+t$Sub_metering_2 <- as.numeric(t$Sub_metering_2) 
+t$Sub_metering_3 <- as.numeric(t$Sub_metering_3) 
+png("plot2.png")
+plot(t$dT,t$Global_active_power,type='n',main = "", xlab="", ylab="Global Active Power (kilowatts)") 
+lines(t$dT,t$Global_active_power, type='l') 
+dev.off()
